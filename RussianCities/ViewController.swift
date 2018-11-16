cdd3674981a2bec0b6d6bb3a564a2aefec438233
Михаил Андреичев <andreichev.m@mail.ru>
2018-11-16 22:56:47 +0300

    Initial Commit

---
+++ RussianCities/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RussianCities
+//
+//  Created by Михаил Андреичев on 16/11/2018.
+//  Copyright © 2018 MichailAndreichev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
